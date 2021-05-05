ed628bb65a79db6920e6cb6474e250f3c6768c2c
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-05-05 11:29:07 +0100

    fix: 9.5 WebBMI

---
+++ Calculator.ts
@@ -0,0 +1,36 @@
+export const calculateBmii = (height: number, weight: number): string => {
+  height = height / 100;
+
+  const bmi: number = weight / (height * height);
+  let bm;
+  switch (true) {
+    case 0 <= bmi && bmi < 15:
+      bm = "Very severely underweight";
+      break;
+    case 15 <= bmi && bmi < 16:
+      bm = "severely underweight";
+      break;
+    case 16 <= bmi && bmi < 18.5:
+      bm = "Underweight";
+      break;
+    case 18.5 <= bmi && bmi < 25:
+      bm = "Normal (healthy weight)";
+      break;
+    case 25 <= bmi && bmi < 30:
+      bm = "Overweight";
+      break;
+    case 30 <= bmi && bmi < 35:
+      bm = "Obese Class I (Moderately obese)";
+      break;
+    case 35 <= bmi && bmi < 40:
+      bm = "Obese Class II (Severely obese)";
+      break;
+    case bmi > 40:
+      bm = "Obese Class III (Very severely obese)";
+      break;
+    default:
+      return "INVALID DETAILS";
+  }
+
+  return bm;
+};
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {calculateBmii} from './Calculator'
 const app = express()
 
 
@@ -15,44 +16,10 @@ app.get("/bmi", (req, res)=>{
     const w:number = Number(req.query.weight)
 
     if(!h || !w)  res.json({error: "malformatted parameters"})
-
-    const calculateBmi = (height: number, weight: number): string => {
-  height = height / 100;
-
-  const bmi: number = weight / (height * height);
-  let bm;
-  switch (true) {
-    case 0 <= bmi && bmi < 15:
-      bm = "Very severely underweight";
-      break;
-    case 15 <= bmi && bmi < 16:
-      bm = "severely underweight";
-      break;
-    case 16 <= bmi && bmi < 18.5:
-      bm = "Underweight";
-      break;
-    case 18.5 <= bmi && bmi < 25:
-      bm = "Normal (healthy weight)";
-      break;
-    case 25 <= bmi && bmi < 30:
-      bm = "Overweight";
-      break;
-    case 30 <= bmi && bmi < 35:
-      bm = "Obese Class I (Moderately obese)";
-      break;
-    case 35 <= bmi && bmi < 40:
-      bm = "Obese Class II (Severely obese)";
-      break;
-    case bmi > 40:
-      bm = "Obese Class III (Very severely obese)";
-      break;
-    default:
-      return "INVALID DETAILS";
-  }
-
-  return bm;
-};
-    res.json({weight:w, height:h, bmi:calculateBmi(h,w)})
+ 
+    calculateBmii(h,w)
+    
+    res.json({weight:w, height:h, bmi:calculateBmii(h,w)})
 })
 const PORT = 3030
 
